665b69ef41d05cf56bd4aa1e9bd046217ebf25a2
hanfarhansifarhan <farhannursahid321@gmail.com>
2026-09-03 20:21:48 +0700

    Perbaiki struktur project Laravel

---
+++ latihan-laravel/resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pemrograman Web II</title>
+</head>
+<body>
+
+    <h1>Farhan Nur Sahid</h1>
+    <h2>H1H024057</h2>
+
+</body>
+</html>
